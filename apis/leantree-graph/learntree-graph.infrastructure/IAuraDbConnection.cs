3a44239917419e03c812afbc08672398d6c8041e
bsaranga <bsaranga@gmail.com>
2022-03-28 09:21:39 +0530

    learn tree api dev

---
+++ apis/leantree-graph/learntree-graph.infrastructure/IAuraDbConnection.cs
@@ -0,0 +1,14 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Neo4j.Driver;
+
+namespace learntree_graph.infrastructure
+{
+    public interface IAuraDbConnection
+    {
+        IAuraDbConnection EstablishConnection();
+        IAsyncSession GetAsyncSession();
+    }
+}
---
+++ apis/leantree-graph/learntree-graph.infrastructure/Repositories/IGraphCore.cs
@@ -0,0 +1,12 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace learntree_graph.infrastructure.Repositories
+{
+    public interface IGraphCore
+    {
+        Task CreateNode(string label, Dictionary<string, string> properties);
+    }
+}
