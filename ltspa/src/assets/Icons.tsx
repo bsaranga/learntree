ffd7259deb92215677b149fe4c8f62fea09cdd4f
bsaranga <bsaranga@gmail.com>
2022-04-23 19:22:10 +0530

    more refactors

--- ltspa/src/assets/Icons.tsx
+++ ltspa/src/assets/Icons.tsx
@@ -23,4 +23,24 @@ function ThumbsDown(props: IPropIcon) {
 	);
 }
 
-export {ThumbsUp, ThumbsDown};
+function ChevronDown(props: IPropIcon) {
+	return (
+		<div className={props.className}>
+			<svg xmlns="http://www.w3.org/2000/svg" className={props.scale} viewBox="0 0 20 20" fill="currentColor">
+				<path fillRule="evenodd" d="M5.293 7.293a1 1 0 011.414 0L10 10.586l3.293-3.293a1 1 0 111.414 1.414l-4 4a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414z" clipRule="evenodd" />
+			</svg>
+		</div>
+	);
+}
+
+function DoubleChevronDown(props: IPropIcon) {
+	return (
+		<div className={props.className}>
+			<svg xmlns="http://www.w3.org/2000/svg" className={props.scale} viewBox="0 0 20 20" fill="currentColor">
+				<path fillRule="evenodd" d="M15.707 4.293a1 1 0 010 1.414l-5 5a1 1 0 01-1.414 0l-5-5a1 1 0 011.414-1.414L10 8.586l4.293-4.293a1 1 0 011.414 0zm0 6a1 1 0 010 1.414l-5 5a1 1 0 01-1.414 0l-5-5a1 1 0 111.414-1.414L10 14.586l4.293-4.293a1 1 0 011.414 0z" clipRule="evenodd" />
+			</svg>
+		</div>
+	);
+}
+
+export {ThumbsUp, ThumbsDown, ChevronDown, DoubleChevronDown};
--- ltspa/src/components/Card.tsx
+++ ltspa/src/components/Card.tsx
@@ -1,4 +1,4 @@
-import { ThumbsDown, ThumbsUp } from '../assets/Icons';
+import { ChevronDown, DoubleChevronDown, ThumbsDown, ThumbsUp } from '../assets/Icons';
 import ICardProps from '../interfaces/ICardProps';
 import './_debug.scss';
 
@@ -10,33 +10,49 @@ export default function Card(props: ICardPropWrapper) {
 	const dProps = {...props.data as ICardProps};
 	return(
 		<div className='bg-white w-1/2 mx-auto rounded-md flex flex-col divide-y shadow-lg'>
+			{/*Header*/}
 			<div className='flex justify-between pt-4 pb-2 px-5'>
 				<div className='flex'>
 					<div className=''>
 						<img className='w-9 h-9 rounded-full ring-2 ring-red-400' src={dProps.author.imageUrl} alt={`Image of ${dProps.author.name}`} />
 					</div>
-					<div className='flex flex-col ml-2 justify-center mt-0.5'>
+					<div className='flex flex-col ml-3 justify-center mt-0.5'>
 						<div className='font-semibold text-lg -mt-1 -mb-1'>{dProps.author.name}</div>
 						<div className='text-xs font-normal text-gray-600'>{dProps.author.title}</div>
 					</div>
 				</div>
-				<div className='text-xs font-medium text-teal-700 self-center'>Follow</div>
+				<button className='text-xs font-medium text-teal-700 hover:text-teal-500 self-center'>Follow</button>
 			</div>
+			{/*Body*/}
 			<div className='py-2 px-8 flex flex-col'>
 				<div>
 					<div className='text-lg -mb-1'>{dProps.lPath.title}</div>
 					<div className='text-xs'>{dProps.lPath.subtitle}</div>
 					<p className='mt-4 text-[0.75rem] mb-4'>{dProps.lPath.description}</p>
-					<div className='text-white font-medium text-xxs2 bg-red-400 w-fit px-1 py-0.5 rounded-sm'>Explore</div>
+					<button className='text-white font-medium text-xxs2 bg-red-400 hover:bg-red-500 px-1 py-0.5 rounded-sm'>Explore</button>
 				</div>
 			</div>
-			<div className='mt-1.5 py-2 px-3'>
-				<div className='flex'>
-					<div className='flex'>
-						<div className='text-xs font-semibold bg-emerald-500'>3</div>
-						<ThumbsUp className="text-gray-300 hover:text-green-400" scale='h-4 w-4'/>
+			{/*Social actions*/}
+			<div className='mt-3 py-2 px-4 flex flex-col'>
+				<div className='flex justify-between mt-2'>
+					<div className='grow mr-2'>
+						<form action='none' className='border-2 rounded-sm flex py-1 px-2'>
+							<input className='w-full text-xxs2' type="text" name='comment' id='comment' placeholder='Comment'></input>
+							<button type='button' className='text-xxs2 font-semibold text-blue-900'>Post</button>
+						</form>
+						<DoubleChevronDown className='text-gray-300 hover:text-green-400 self-center' scale='h-4 w-4'/>
 					</div>
-					<ThumbsDown className="text-gray-300 hover:text-red-400" scale='h-4 w-4'/>
+					<div className='flex items-center space-x-2'>
+						<div className='flex'>
+							<div className='text-xs font-semibold px-0.5 rounded-sm text-green-500'>{dProps.votes.upvotes}</div>
+							<ThumbsUp className="text-gray-300 hover:text-green-400 ml-0.5" scale='h-4 w-4'/>
+						</div>
+						<div className='flex'>
+							<div className='text-xs font-semibold px-0.5 rounded-sm text-red-500'>{dProps.votes.downvotes}</div>
+							<ThumbsDown className="text-gray-300 hover:text-red-400" scale='h-4 w-4'/>
+						</div>
+						<button className='text-xs ml-2 bg-red-600 hover:bg-red-500 text-white px-2 py-1 rounded-sm'>Subscribe</button>
+					</div>
 				</div>
 			</div>
 		</div>
