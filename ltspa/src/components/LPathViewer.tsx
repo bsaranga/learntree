c39a8046ac339aa33915108b28ed9c1a1a241096
bsaranga <bsaranga@gmail.com>
2022-05-02 14:05:43 +0530

    Added learning path layout

--- ltspa/src/components/LPathViewer.tsx
+++ ltspa/src/components/LPathViewer.tsx
@@ -1,55 +1,192 @@
-import { useLayoutEffect, useRef } from 'react';
+import { useEffect, useRef } from 'react';
 import { useParams } from 'react-router-dom';
-import G6 from '@antv/g6';
-import { findDOMNode } from 'react-dom';
+import G6, { NodeConfig, TreeGraph } from '@antv/g6';
 
-const data = {
-	// The array of nodes
-	nodes: [
-		{
-			id: 'node1', // String, unique and required
-			x: 100, // Number, the x coordinate
-			y: 200, // Number, the y coordinate
-		},
-		{
-			id: 'node2', // String, unique and required
-			x: 300, // Number, the x coordinate
-			y: 200, // Number, the y coordinate
-		},
-	],
-	// The array of edges
-	edges: [
-		{
-			source: 'node1', // String, required, the id of the source node
-			target: 'node2', // String, required, the id of the target node
-		},
-	],
-};
 
 export default function LPathViewer() {
+	const dendoGramData = {
+		'id': 'Modeling Methods',
+		'children': [
+			{
+				'id': 'Classification',
+				'children': [
+					{
+						'id': 'Logistic regression'
+					},
+					{
+						'id': 'Linear discriminant analysis'
+					},
+					{
+						'id': 'Rules'
+					},
+					{
+						'id': 'Decision trees'
+					},
+					{
+						'id': 'Naive Bayes'
+					},
+					{
+						'id': 'K nearest neighbor'
+					},
+					{
+						'id': 'Probabilistic neural network'
+					},
+					{
+						'id': 'Support vector machine'
+					}
+				]
+			},
+			{
+				'id': 'Consensus',
+				'children': [
+					{
+						'id': 'Models diversity',
+						'children': [
+							{
+								'id': 'Different initializations'
+							},
+							{
+								'id': 'Different parameter choices'
+							},
+							{
+								'id': 'Different architectures'
+							},
+							{
+								'id': 'Different modeling methods'
+							},
+							{
+								'id': 'Different training sets'
+							},
+							{
+								'id': 'Different feature sets'
+							}
+						]
+					},
+					{
+						'id': 'Methods',
+						'children': [
+							{
+								'id': 'Classifier selection'
+							},
+							{
+								'id': 'Classifier fusion'
+							}
+						]
+					},
+					{
+						'id': 'Common',
+						'children': [
+							{
+								'id': 'Bagging'
+							},
+							{
+								'id': 'Boosting'
+							},
+							{
+								'id': 'AdaBoost'
+							}
+						]
+					}
+				]
+			},
+			{
+				'id': 'Regression',
+				'children': [
+					{
+						'id': 'Multiple linear regression'
+					},
+					{
+						'id': 'Partial least squares'
+					},
+					{
+						'id': 'Multi-layer feedforward neural network'
+					},
+					{
+						'id': 'General regression neural network'
+					},
+					{
+						'id': 'Support vector regression'
+					}
+				]
+			}
+		]
+	};
+
 	const params = useParams();
 
 	const ref = useRef<HTMLDivElement>(null);
-	let graph = null;
-	let grid = null;
 
-	useLayoutEffect(() => {
-		grid = new G6.Grid({});
-		graph = new G6.Graph({
+	useEffect(() => {
+		console.log('Graph render');
+		
+		const grid = new G6.Grid();
+		const graph: TreeGraph = new G6.TreeGraph({
 			container: ref.current as HTMLDivElement,
 			fitView: true,
+			fitViewPadding: 50,
 			modes: {
-				default: ['drag-canvas', 'zoom-canvas'],
+				default: ['drag-canvas', 'zoom-canvas', 'drag-node'],
+			},
+			defaultEdge: {
+				type: 'cubic-horizontal',
+			},
+			layout: {
+				type: 'mindmap',
+				direction: 'H',
+				getHeight: () => {
+					return 12;
+				},
+				getWidth: () => {
+					return 12;
+				},
+				getVGap: () => {
+					return 8;
+				},
+				getHGap: () => {
+					return 60;
+				},
+				getSide: (node: NodeConfig) => {
+					if (node.id === 'Classification') {
+						return 'left';
+					}
+					return 'right';
+				},
+			},
+			animate: true,
+			animateCfg: {
+				duration: 500,
+				easing: 'easeCubic'
 			},
 			plugins: [grid]
 		});
 
-		graph.data(data);
+		graph.node((node: NodeConfig) => {
+			let centerNode = 0;
+			if (node?.id === 'Modeling Methods') {
+				centerNode = node?.x as number;
+			}
+
+			return {
+				label: node?.id,
+				labelCfg: {
+					style: {
+						fontFamily: 'ui-sans-serif, system-ui, -apple-system, BlinkMacSystemFont, "Segoe UI", Roboto, "Helvetica Neue", Arial, "Noto Sans", sans-serif, "Apple Color Emoji", "Segoe UI Emoji", "Segoe UI Symbol", "Noto Color Emoji"',
+						fontWeight: 400,
+						fontSize: 12
+					},
+					position: (!node.children) ? ((node.x as number) > centerNode ? 'right' : 'left') : 'top'
+				}
+			};
+		});
+
+		graph.data(dendoGramData);
 		graph.render();
+
+		return () => graph.destroy();
 	}, []);
 
 	return (
-		<div ref={ref} className="w-[100vw] h-[95vh]">
+		<div ref={ref} className="w-[100vw] h-[90vh]">
 			
 		</div>
 	);
--- ltspa/src/components/Layout.tsx
+++ ltspa/src/components/Layout.tsx
@@ -1,16 +1,18 @@
-import { Outlet } from 'react-router-dom';
+import { Outlet, useLocation } from 'react-router-dom';
 import MessageHubContext from '../contexts/MessageHubContext';
 import { GetHubConnection, StartHub } from '../services/MessageHub';
 import { Header } from './Header';
 
 export default function Layout() {
+	const loc = useLocation();
+	const lPathMatch = loc.pathname.split('/').filter(p => p!='').indexOf('lpath') == 0;
 	const messageHub = GetHubConnection();
 	StartHub(messageHub);
 	return (
 		<>
 			<Header/>
 			<MessageHubContext.Provider value={messageHub}>
-				<div className="bg-slate-200 h-[95vh] overflow-y-auto">
+				<div className={lPathMatch ? 'h-[93vh] overflow-y-auto overflow-x-hidden' : 'bg-slate-200 h-[93vh] overflow-y-auto'}>
 					<div className='flex justify-center'>
 						<Outlet/>
 					</div>
