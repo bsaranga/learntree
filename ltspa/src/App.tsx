356410c29a00af4581e497eea18c2c294c989249
bsaranga <bsaranga@gmail.com>
2022-05-07 21:22:01 +0530

    added routes for lpath creator

--- ltspa/src/App.tsx
+++ ltspa/src/App.tsx
@@ -1,6 +1,7 @@
 import { Route, Routes } from 'react-router-dom';
 import './App.scss';
 import Layout from './components/Layout';
+import LPathCreator from './components/LPathCreator';
 import LPathViewer from './components/LPathViewer';
 import { NewsFeed } from './pages/NewsFeed';
 import NotFound from './pages/NotFound';
@@ -13,6 +14,7 @@ function App() {
 				<Route index element={<NewsFeed/>}/>
 				<Route path='profile' element={<Profile/>} />
 				<Route path='lpath/:lPathId' element={<LPathViewer/>}/>
+				<Route path='create' element={<LPathCreator/>}/>
 				<Route path='*' element={<NotFound/>} />
 			</Route>
 		</Routes>
---
+++ ltspa/src/components/LPathCreator.tsx
@@ -0,0 +1,5 @@
+export default function LPathCreator() {
+	return (
+		<div className="text-lg text-red-600 font-bold">LPath Creator</div>
+	);
+}
