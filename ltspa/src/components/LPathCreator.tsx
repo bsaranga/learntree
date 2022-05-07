67c0cb74003fe4f089b7c77f9d8024bf52aa91db
bsaranga <bsaranga@gmail.com>
2022-05-07 23:00:26 +0530

    added context menu

--- ltspa/src/components/LPathCreator.tsx
+++ ltspa/src/components/LPathCreator.tsx
@@ -1,5 +1,85 @@
+import G6, { Graph, GraphData } from '@antv/g6';
+import { useLayoutEffect, useRef } from 'react';
+import { randomIdGenerator } from '../utilities/generators';
+import './Layout.scss';
+import './Context.scss';
+
 export default function LPathCreator() {
+	const ref = useRef<HTMLDivElement>(null);
+	const data: GraphData = {
+		nodes: [
+			{
+				id: randomIdGenerator(),
+				label: 'Node 1',
+				x: 500,
+				y: 500
+			}
+		]
+	};
+
+	useLayoutEffect(() => {
+		const grid = new G6.Grid();
+		const contextMenu = new G6.Menu({
+			getContent(evt) {
+				console.log(evt);
+				return `<h3>My Context</h3>
+                <ul>
+                    <li title='1'>li 1</li>
+                    <li title='2'>li 2</li>
+                    <li>li 3</li>
+                    <li>li 4</li>
+                    <li>li 5</li>
+                </ul>`;
+			},
+			handleMenuClick: (target, item) => {
+				console.log(target, item);
+			},
+			offsetX: 0,
+			offsetY: 0,
+			className: 'myContextClass',
+			itemTypes: ['canvas'],
+		});
+
+		const graph: Graph = new G6.Graph({
+			container: ref.current as HTMLDivElement,
+			fitView: true,
+			fitViewPadding: 50,
+			modes: {
+				default: ['drag-canvas', 'zoom-canvas', 'drag-node', 'brush-select']
+			},
+			defaultNode: {
+				size: 50
+			},
+			animate: true,
+			animateCfg: {
+				duration: 450,
+				easing: 'easeCubic'
+			},
+			plugins: [grid, contextMenu]
+		});
+
+		graph.data(data);
+		graph.render();
+
+		// Handlers
+		function handleResize() {
+			graph.changeSize(ref.current?.clientWidth as number, ref.current?.clientHeight as number);
+		}
+
+		// DOM Event Listeners
+		window.addEventListener('resize', handleResize);
+
+		return () => {
+			graph.destroy();
+			window.removeEventListener('resize', handleResize);
+		};
+	}, []);
+
 	return (
-		<div className="text-lg text-red-600 font-bold">LPath Creator</div>
+		<div ref={ref} className="w-[100vw] dynamicHeight">
+			<div className='p-2 absolute'>
+				<div className='text-sm font-medium text-slate-700'>Create Mode</div>
+			</div>
+		</div>
 	);
 }
--- ltspa/src/components/Layout.tsx
+++ ltspa/src/components/Layout.tsx
@@ -8,6 +8,7 @@ import './Layout.scss';
 export default function Layout() {
 	const loc = useLocation();
 	const lPathMatch = loc.pathname.split('/').filter(p => p!='').indexOf('lpath') == 0;
+	const createPathMatch = loc.pathname.split('/').filter(p => p!='').indexOf('create') == 0;
 	const messageHub = GetHubConnection();
 	StartHub(messageHub);
 	return (
@@ -15,7 +16,7 @@ export default function Layout() {
 			<Header/>
 			<MessageHubContext.Provider value={messageHub}>
 				<APIContext.Provider value={APIContextValue}>
-					<div className={lPathMatch ? 'dynamicHeight overflow-y-auto overflow-x-hidden' : 'bg-slate-200 dynamicHeight overflow-y-auto'}>
+					<div className={(lPathMatch || createPathMatch) ? 'dynamicHeight overflow-y-auto overflow-x-hidden' : 'bg-slate-200 dynamicHeight overflow-y-auto'}>
 						<div className='flex justify-center'>
 							<Outlet/>
 						</div>
