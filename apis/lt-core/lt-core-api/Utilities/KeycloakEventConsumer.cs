b27d3eace9461016ac6e617e40b8853ace40c94b
bsaranga <bsaranga@gmail.com>
2022-05-24 08:17:49 +0530

    sample routing keys and rabbitmq configs

--- apis/lt-core/lt-core-api/Utilities/KeycloakEventConsumer.cs
+++ apis/lt-core/lt-core-api/Utilities/KeycloakEventConsumer.cs
@@ -8,30 +8,38 @@ namespace lt_core_api.Utilities
         private readonly ConnectionFactory rabbitmqConnectionFactory;
         private readonly IConnection rabbitmqConnection;
         private readonly IModel rabbitmqChannel;
+        private readonly string keycloakTopicExchange = "";
 
         private readonly ILogger<KeycloakEventConsumer> _logger;
 
-        public KeycloakEventConsumer(ILogger<KeycloakEventConsumer> logger)
+        public KeycloakEventConsumer(ILogger<KeycloakEventConsumer> logger, IConfiguration configuration)
         {
             _logger = logger;
 
             this.rabbitmqConnectionFactory = new ConnectionFactory()
             {
-                HostName = "localhost", 
-                Port = 5673, 
-                VirtualHost = "/",
-                UserName = "guest",
-                Password = "guest"
+                HostName = configuration.GetSection("RabbitMQ:Host").Value, 
+                Port = Int32.Parse(configuration.GetSection("RabbitMQ:Port").Value), 
+                VirtualHost = configuration.GetSection("RabbitMQ:VirtualHost").Value,
+                UserName = configuration.GetSection("RabbitMQ:User").Value,
+                Password = configuration.GetSection("RabbitMQ:Password").Value
             };
+
+            keycloakTopicExchange = configuration.GetSection("KeycloakEvents:KC_Topic_Exchange").Value;
 
             this.rabbitmqConnection = this.rabbitmqConnectionFactory.CreateConnection("lt-core-api:keycloak-event-consumer");
             this.rabbitmqChannel = this.rabbitmqConnection.CreateModel();
 
-            this.rabbitmqChannel.ExchangeDeclare("keycloak-topic-exchange", ExchangeType.Topic, true, false, null);
+            this.rabbitmqChannel.ExchangeDeclare(keycloakTopicExchange, ExchangeType.Topic, true, false, null);
             this.rabbitmqChannel.QueueDeclare("learntree-all-events", true, false, false, null);
-            this.rabbitmqChannel.QueueBind("learntree-all-events", "keycloak-topic-exchange", "KK.EVENT.*.LearnTree.#", null);
+            this.rabbitmqChannel.QueueBind("learntree-all-events", keycloakTopicExchange, "KK.EVENT.*.LearnTree.#", null);
 
             _logger.LogInformation($"[Keycloak Event Consumer Established] {rabbitmqConnection.ClientProvidedName}: {rabbitmqConnection.Endpoint.ToString()}");
+            _logger.LogInformation($"GOT TOPIC EXCHANGE FROM CONFIG: {keycloakTopicExchange}");
+        }
+
+        public void DeclareUserRegistrationQueue() {
+
         }
     }
 }
