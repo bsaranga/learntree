9b0f799d20d69a2961b7064585997310273cee1b
bsaranga <bsaranga@gmail.com>
2022-06-10 08:58:27 +0530

    Removed logs from git

--- apis/lt-message-hub/message-hub.api/Program.cs
+++ apis/lt-message-hub/message-hub.api/Program.cs
@@ -1,38 +1,60 @@
 using System.Text.Json;
 using message_hub.api;
+using Serilog;
+using Serilog.Events;
 
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
+Log.Logger = new LoggerConfiguration()
+            .ReadFrom.Configuration(builder.Configuration, "Serilog")
+            //.WriteTo.Seq("http://localhost:5341", apiKey: "lAloTffsLMW4TIA7jBN3")
+            .CreateLogger();
 
-builder.Services.AddControllers();
-builder.Services.AddSignalR()
-    .AddJsonProtocol(conf => conf.PayloadSerializerOptions.PropertyNamingPolicy = JsonNamingPolicy.CamelCase );
-
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
-
-var app = builder.Build();
-
-// Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
+try
 {
-    app.UseSwagger();
-    app.UseSwaggerUI();
+    Log.Information("Starting web host");
+
+    builder.Services.AddControllers();
+    
+    builder.Services.AddSignalR()
+        .AddJsonProtocol(conf => conf.PayloadSerializerOptions.PropertyNamingPolicy = JsonNamingPolicy.CamelCase);
+
+    builder.Services.AddEndpointsApiExplorer();
+
+    builder.Services.AddSwaggerGen();
+
+    builder.Host.UseSerilog();
+
+    var app = builder.Build();
+
+    // Configure the HTTP request pipeline.
+    if (app.Environment.IsDevelopment())
+    {
+        app.UseSwagger();
+        app.UseSwaggerUI();
+    }
+
+    app.UseHttpsRedirection();
+
+    app.UseCors(p => p.AllowAnyHeader()
+                      .AllowAnyMethod()
+                      .AllowCredentials()
+                      .WithOrigins("https://localhost:3000"));
+
+    app.UseAuthorization();
+
+    app.MapControllers();
+
+    app.MapHub<MessageHub>("/messagehub");
+
+    app.Run();
+
+} catch (Exception ex)
+{
+    Log.Fatal(ex, "Host terminated unexpectedly");
+
+} finally
+{
+    Log.CloseAndFlush();
+
 }
-
-app.UseHttpsRedirection();
-
-app.UseCors(p => p.AllowAnyHeader()
-                  .AllowAnyMethod()
-                  .AllowCredentials()
-                  .WithOrigins("https://localhost:3000"));
-
-app.UseAuthorization();
-
-app.MapControllers();
-
-app.MapHub<MessageHub>("/messagehub");
-
-app.Run();
