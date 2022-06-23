46a31e9082efd92d01e3eed08618e37f62f25975
bsaranga <bsaranga@gmail.com>
2022-06-23 23:03:28 +0530

    filterable topic list

--- ltspa/src/components/Newsfeed/Interests/InterestsModal.tsx
+++ ltspa/src/components/Newsfeed/Interests/InterestsModal.tsx
@@ -1,4 +1,4 @@
-import { Modal, Select } from 'antd';
+import { message, Modal, Select } from 'antd';
 import { useContext, useEffect, useState } from 'react';
 import { useDispatch } from 'react-redux';
 import HttpContext from '../../../contexts/HttpContext';
@@ -9,6 +9,7 @@ import { setIfFirstLogin } from '../../../store/slices/rootSlice';
 export default function InterestsModal() {
 
 	const [topics, setTopics] = useState<Topic[] | null>(null);
+	const [selectedTopics, setSelectedTopics] = useState<number[]>([]);
 	const httpContext = useContext(HttpContext);
 
 	useEffect(() => {
@@ -29,17 +30,27 @@ export default function InterestsModal() {
 		dispatch(setIfFirstLogin(false));
 	}
 
-	function onOkInterestsModal() {
-		console.log('Interests registered...');
+	async function onOkInterestsModal() {
+		const res = await httpContext.post<number[]>('https://localhost:4157/api/user/usertopic', selectedTopics);
+		if (res.status == 200) {
+			message.success({ content: 'Interests registered', duration: 2, style: { marginTop: '2rem' } });
+			dispatch(setIfFirstLogin(false));
+		}
+	}
+
+	function onSelect(selected: string[]) {
+		setSelectedTopics(selected.map(str => Number(str)));
 	}
 
 	return <Modal title="Pick your interests" visible={isFirstLogin} onCancel={onCloseInterestsModal} width={640} centered={true} destroyOnClose={true} footer={(() => <button onClick={onOkInterestsModal} className='ring-2 ring-blue-300 hover:ring-blue-500 active:ring-blue-400 px-[.64rem] py-[.2rem] text-xs rounded-sm focus:outline-none'>Accept</button>)()}>
 		<Select
 			mode="multiple"
+			optionFilterProp='children'
+			filterOption={(input, option) => (option?.children as unknown as string).toLowerCase().includes(input.toLowerCase())}
 			allowClear
 			style={{ width: '100%' }}
-			placeholder="Please select"
-			onChange={(event) => console.log(event)}
+			placeholder="Select your interests"
+			onChange={(sel) => onSelect(sel)}
 		>
 			{optionsList}
 		</Select>
