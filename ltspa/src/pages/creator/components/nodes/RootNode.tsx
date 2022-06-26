7601aed8f6169f75e8bf02e7f382ed23d3e6a2d7
bsaranga <bsaranga@gmail.com>
2022-06-26 12:38:58 +0530

    a bit cleaner node

--- ltspa/src/pages/creator/components/nodes/RootNode.tsx
+++ ltspa/src/pages/creator/components/nodes/RootNode.tsx
@@ -1,10 +1,18 @@
+import { message } from 'antd';
 import { KeyboardEvent, useCallback, useEffect, useMemo, useRef, useState } from 'react';
 import { Handle, Position } from 'react-flow-renderer';
 
 export default function RootNode() {
-	const [nodeText, setNodeText] = useState<string>('Untitled Root Node');
+	const [nodeText, setNodeText] = useState<string>('Untitled');
 	const [nodeInit, setNodeInit] = useState<boolean>(false);
+
 	const inputRef = useRef<HTMLInputElement>(null);
+
+	const nodeRef = useCallback((node: HTMLDivElement) => {
+		if (node != null && nodeInit == true && inputRef.current != null) {
+			inputRef.current.style.width = `${node.clientWidth - 16}px`;
+		}
+	}, [nodeInit]);
 
 	useEffect(() => {
 		let timeOut: NodeJS.Timeout;
@@ -38,27 +46,36 @@ export default function RootNode() {
 		if (event.code != 'Enter' && event.code != 'NumpadEnter') {
 			setNodeText((event.target as HTMLInputElement).value);
 		} else {
-			initialize();
+			if (nodeText.length > 0) {
+				initialize();
+			} else {
+				message.error({ content: 'Please enter a node label', duration: 2, style: { marginTop: '2rem' } });
+			}
 		}
-	}, []);
+	}, [nodeText]);
 
 	const editHandler = useCallback(() => {
 		unInitialize();
 	}, []);
 
 	const blurHandler = useCallback(() => {
-		initialize();
-	}, []);
+		if (nodeText.length > 0) {
+			initialize();
+		} else {
+			setNodeText('Untitled');
+			initialize();
+		}
+	}, [nodeText]);
 
 	useMemo(() => console.log('Rendered Root Node'), []);
 
 	return (
 		<>
-			<div className='bg-white p-2 rounded-md border-2 border-gray-300' onDoubleClick={editHandler}>
-				<input ref={inputRef} type="text" onKeyUp={onChange} onBlur={blurHandler} />
-				{nodeInit && nodeText}
+			<div ref={nodeRef} className='bg-white p-2 rounded-md border-2 border-gray-400 shadow-sm' onDoubleClick={editHandler}>
+				<input ref={inputRef} type="text" onKeyUp={onChange} onBlur={blurHandler} className="h-[18px] focus:outline-none focus:border-b-2 focus:border-b-red-400" />
+				{nodeInit && <div>{nodeText}</div>}
 			</div>
-			<Handle type="source" style={{width: '8px', height: '8px'}} position={Position.Bottom} id="a" />
+			<Handle type="source" style={{width: '10px', height: '10px'}} position={Position.Bottom} id="a" />
 		</>
 	);
 }
