b67d817ed02b63775c6d3cd8c60d77bd0b22ccd1
bsaranga <bsaranga@gmail.com>
2022-06-29 23:02:54 +0530

    toolbar + title card

--- ltspa/src/pages/creator/Creator.tsx
+++ ltspa/src/pages/creator/Creator.tsx
@@ -4,6 +4,9 @@ import PaneContextMenu from './components/PaneContextMenu';
 import ContextMenuMetaData from './interfaces/ContextMenuMetaData';
 import RootNode from './components/nodes/RootNode';
 import TopicNode from './components/nodes/TopicNode';
+import { Modal } from 'antd';
+import CreateLPForm from '../../components/LPathCreator/CreateLPForm/CreateLPForm';
+import { useNavigate } from 'react-router-dom';
 
 interface EdgeInfo {
 	id?: string,
@@ -21,12 +24,15 @@ interface HoveredEdge {
 }
 
 export default function Creator() {
+	const navigate = useNavigate();
 	const heightPadding = 3; // rem
 	const [nodes, setNodes] = useState<Node[]>([]);
 	const [edges, setEdges1] = useState<Edge[]>([]);
 	const [contextMenuVisible, setContextMenuVisibility] = useState<boolean>(false);
 	const [contextMenuMetaData, setContextMenuMetaData] = useState<ContextMenuMetaData>({});
 	const [activeEdgeInfo, setActiveEdgeInfo] = useState<EdgeInfo>({ active: false });
+
+	const [createModalVisible, setCreateModalVisibility] = useState(true);
 	
 	const {getNodes, addNodes, getEdges, setEdges, project, toObject} = useReactFlow();
 	const nodeTypes = useMemo(() => ({ root: RootNode, topic: TopicNode }), []);
@@ -82,16 +88,35 @@ export default function Creator() {
 		console.log(toObject());
 	}, []);
 
+	function navigateHome() {
+		navigate('/');
+	}
+
+	function modalOkHandler() {
+		setCreateModalVisibility(false);
+	}
+
+	function modalCancelHandler() {
+		setCreateModalVisibility(false);
+		navigateHome();
+	}
+
 	useMemo(() => console.log('Creator rendered...'), []);
 
 	return (
 		<>
+			<Modal title="Create Learning Path" visible={createModalVisible} width={450} centered={true} destroyOnClose={true} transitionName='' closable={false} footer={null}>
+				<CreateLPForm onCancel={modalCancelHandler} onOk={modalOkHandler}/>
+			</Modal>
+			<div className='absolute z-10 mb-12 bottom-6 left-6 bg-slate-50 bg-opacity-50 hover:bg-opacity-100 transition-color duration-200 px-2 rounded-sm shadow-sm'>
+				<div className='select-none text-base font-semibold text-slate-700'>Learning Path Title</div>
+			</div>
 			<button className='absolute bg-white z-10 border-2 border-gray-500 shadow-md rounded-md p-2 focus:outline-none' onClick={() => console.log(toObject())}>Show Context</button>
 			{/*-----------------------------------------------------------------------------------------*/}
 			<div className='absolute z-10 bg-white bottom-6 left-6 flex p-2 justify-between space-x-4 rounded-sm px-4 shadow-md'>
-				<button className='bg-slate-400 px-2 py-1 focus:outline-none rounded-sm text-xs font-semibold text-white hover:bg-slate-300 active:bg-slate-400'>Undo</button>
-				<button className='bg-slate-400 px-2 py-1 focus:outline-none rounded-sm text-xs font-semibold text-white hover:bg-slate-300 active:bg-slate-400'>Redo</button>
-				<button className='bg-slate-400 px-2 py-1 focus:outline-none rounded-sm text-xs font-semibold text-white hover:bg-slate-300 active:bg-slate-400' onClick={saveHandler}>Save</button>
+				<button className='bg-slate-400 px-2 py-1 focus:outline-none rounded-sm text-xs font-semibold text-white hover:bg-slate-300 active:bg-slate-400 transition-color duration-100'>Undo</button>
+				<button className='bg-slate-400 px-2 py-1 focus:outline-none rounded-sm text-xs font-semibold text-white hover:bg-slate-300 active:bg-slate-400 transition-color duration-100'>Redo</button>
+				<button className='bg-slate-400 px-2 py-1 focus:outline-none rounded-sm text-xs font-semibold text-white hover:bg-slate-300 active:bg-slate-400 transition-color duration-100' onClick={saveHandler}>Save</button>
 			</div>
 			{ activeEdgeInfo.active && 
 				<div className='absolute z-10' style={{top: `${activeEdgeInfo.textBoxLocation?.y}px`, left: `${activeEdgeInfo.textBoxLocation?.x}px`}}>
